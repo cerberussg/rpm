4a73fc5ef9278cd2639ba1866fac7788c118c491
Scott Goyette <scott.goyette@rubypythmore.com>
2025-11-10 11:42:57 -0500

    tom got jacked, jack yo body.

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,15 +3,12 @@ import TomSelect from "tom-select"
 
 export default class extends Controller {
   connect() {
-    // Defer initialization to improve INP
-    requestIdleCallback(() => {
-      new TomSelect(this.element, {
-        plugins: ['remove_button'],
-        maxItems: 4,
-        placeholder: 'Select categories (up to 4)...',
-        closeAfterSelect: false,
-        persist: false
-      })
-    }, { timeout: 500 })
+    new TomSelect(this.element, {
+      plugins: ['remove_button'],
+      maxItems: 4,
+      placeholder: 'Select categories (up to 4)...',
+      closeAfterSelect: false,
+      persist: false
+    })
   }
 }
