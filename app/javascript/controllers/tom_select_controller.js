fd7e0e9e45d87a6900778209e0c04de9e1cf2948
Scott Goyette <scott.goyette@rubypythmore.com>
2025-11-09 21:43:29 -0500

    Add Tom Select for multi-category dropdown in admin

---
+++ app/javascript/controllers/tom_select_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select"
+
+export default class extends Controller {
+  connect() {
+    new TomSelect(this.element, {
+      plugins: ['remove_button'],
+      maxItems: 4,
+      placeholder: 'Select categories (up to 4)...',
+      closeAfterSelect: false,
+      persist: false
+    })
+  }
+}
